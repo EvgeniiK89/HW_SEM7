d9a5bbc5c5fbd7040edb8cb1f21a65582f05c72e
EvgeniiK <kachaloff89@gmail.com>
2023-06-13 17:07:45 +0300

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Напишите программу, которая на вход принимает значение элемента в двумерном массиве, 
+// и возвращает позицию этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = Convert.ToInt32(new Random().Next(0, 99));
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + "\t  ");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите размер строки массива: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размер столбца массива: ");
+int col = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[row, col];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine("Введите координату cтроки: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координату столбца: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > row && y > col)
+    Console.WriteLine("Такого числа в массиве нет");
+else
+{
+    object valueOfIndex = array.GetValue(x, y);
+    Console.WriteLine($"Значение вводимого индекса координат: {valueOfIndex}");
+}
